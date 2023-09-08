47fcc296717d310a18757ab184c5526909f52309
Ashennwitch <119783785+Ashennwitch@users.noreply.github.com>
2023-09-08 11:01:23 +0700

    CS01 - Menyelesaikan CS Modul 1

---
+++ JBus/HanifNurIlhamSanjayaJBusBR/JBus.java
@@ -0,0 +1,65 @@
+package JBus.HanifNurIlhamSanjayaJBusBR;
+
+
+/**
+ * Write a description of class JBus here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JBus {
+
+    public int getBusId() {
+        return 0;
+    }
+
+    public String getBusName() {
+        return "Bus";
+    }
+
+    public boolean isDiscount() {
+        return true;
+    }
+
+    public float getDiscountPercentage(int beforeDiscount, int afterDiscount) {
+
+        if (beforeDiscount <= afterDiscount) {
+            return 0.0f; 
+        }
+  
+        float discountPercentage = ((beforeDiscount - afterDiscount) * 100.0f) / beforeDiscount;
+        return discountPercentage;
+    }
+
+    public float getDiscountedPrice(int price, float discountPercentage) {
+
+        if (discountPercentage > 100.0f) {
+            discountPercentage = 100.0f; 
+        }
+
+        float discountedPrice = price * (1 - discountPercentage / 100.0f);
+        return discountedPrice;
+    }
+
+    public int getOriginalPrice(int discountedPrice, float discountPercentage) {
+
+        int originalPrice = (int)(discountedPrice / (1 - discountPercentage / 100.0f));
+        return originalPrice;
+    }
+
+    public float getAdminFeePercentage() {
+        return 0.05f;
+    }
+
+    public int getAdminFee(int price) {
+      
+        float adminFeePercentage = getAdminFeePercentage();
+        int adminFee = (int)(price * adminFeePercentage);
+        return adminFee;
+    }
+
+    public int getTotalPrice(int price, int numberOfSeat) {
+        int totalPrice = (price * numberOfSeat) + getAdminFee(price * numberOfSeat);
+        return totalPrice;
+    }
+}
