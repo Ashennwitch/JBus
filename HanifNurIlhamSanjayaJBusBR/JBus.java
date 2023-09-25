59e029adfc7bfd147aa4f69155c23779c630efe7
Ashennwitch <119783785+Ashennwitch@users.noreply.github.com>
2023-09-25 22:53:05 +0700

    PT03 - Mengupdate atribut main

--- HanifNurIlhamSanjayaJBusBR/JBus.java
+++ HanifNurIlhamSanjayaJBusBR/JBus.java
@@ -23,10 +23,12 @@ public class JBus {
         System.out.println(testStation.print()); 
         */
         
+
+        //Testing
         Review testReview = new Review(1, "23 August 2023", "Bad Quality");
-        Price testPrice = new Price (100088, 20088);
-        Station testDeparture = new Station (2, "Depok Terminal", City.DEPOK, "JL. Margonda Raya");
-        Station testArrival = new Station (3, "Halte UI", City.JAKARTA, "Universitas Indonesia");
+        Price testPrice = new Price(100000, 20000);
+        Station testDeparture = new Station(2, "Depok Terminal", City.DEPOK, "Jl. Margonda Raya");
+        Station testArrival = new Station(3, "Halte UI", City.JAKARTA, "Universitas Indonesia");
         Bus testBus = new Bus(1, "Busway", Facility.AC, testPrice, 50, BusType.REGULER, City.DEPOK, testDeparture, testArrival);
         Account testAccount = new Account(1, "Bob", "bob@gmail.com", "bob");
         Rating testRating = new Rating();
@@ -35,6 +37,7 @@ public class JBus {
         System.out.println(testAccount);
         System.out.println(testPrice);
         System.out.println(testRating);
+    
     }
     
   // public static Bus createBus() {
