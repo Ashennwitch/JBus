9a4434c69ab9471e6f84542ddcb7267554d8c9d2
Ashennwitch <119783785+Ashennwitch@users.noreply.github.com>
2023-10-05 21:13:28 +0700

    CS05 - Menyelesaikan Pre CCCS Modul 5

--- HanifNurIlhamSanjayaJBusBR/Serializable.java
+++ HanifNurIlhamSanjayaJBusBR/Serializable.java
@@ -1,5 +1,6 @@
 package HanifNurIlhamSanjayaJBusBR;
 
+import java.util.HashMap;
 
 /**
  * Write a description of class Serializable here.
@@ -7,13 +8,43 @@ package HanifNurIlhamSanjayaJBusBR;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Serializable
+
+public class Serializable implements Comparable<Serializable>
 {
     public final int id;
+    private static HashMap<Class<?>, Integer> mapCounter = new HashMap<>();
 
-    public Serializable(int id)
+    protected Serializable(int id)
     {
-        // initialise instance variables
         this.id = id;
+        Class<?> clazz = this.getClass();
+        if (!mapCounter.containsKey(clazz) || id >= mapCounter.get(clazz)) {
+            mapCounter.put(clazz, id + 1);
+        }
+    }
+
+    public static <T> void setLastAssignedId(Class<T> clazz, int lastId) {
+        mapCounter.put(clazz, lastId);
+    }
+
+    public static <T> Integer getLastAssignedId(Class<T> clazz) {
+        return mapCounter.get(clazz);
+    }
+
+    @Override
+    public int compareTo(Serializable other) {
+        return Integer.compare(this.id, other.id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Serializable) {
+            return this.id == ((Serializable)obj).id;
+        }
+        return false;
+    }
+
+    public boolean equals(Serializable other) {
+        return this.id == other.id;
     }
 }
