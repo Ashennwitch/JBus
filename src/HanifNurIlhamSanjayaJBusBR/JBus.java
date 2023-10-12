2b3ebafe17533e02a7696767467c7bfc4793a112
Ashennwitch <119783785+Ashennwitch@users.noreply.github.com>
2023-10-12 12:13:25 +0700

    TP06 - Menyelesaikan TP Modul 6

--- src/HanifNurIlhamSanjayaJBusBR/JBus.java
+++ src/HanifNurIlhamSanjayaJBusBR/JBus.java
@@ -5,7 +5,12 @@ import java.util.List;
 import java.util.Map;
 import java.sql.Timestamp;
 import java.util.Arrays;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * Write a description of class JBus here.
@@ -16,7 +21,22 @@ import java.util.Arrays;
 public class JBus {
 
     public static void main(String[] args) {
-        // PT Modul 5
+        // TP Modul 6
+        String filepath = "C:\\Users\\Hanif\\Documents\\DEV\\Praktikum OOP\\JBus\\data\\station.json";
+        Gson gson = new Gson();
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader (filepath));
+            List<Station> stationjson = gson.fromJson(buffer, new TypeToken<List<Station>>() {}.getType());
+            stationjson.forEach(e -> System.out.println(e.toString()));
+            System.out.println();
+            buffer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    /*    // PT Modul 5
         // Tes Pagination
         Bus b = createBus();
         List<Timestamp> listOfSchedules = new ArrayList<>();
@@ -58,7 +78,7 @@ public class JBus {
         System.out.println("\nUpdated Schedule");
         Algorithm.paginate(b.schedules, 0, 4, t-> true).forEach(System.out::println);
     }
-
+*/
     public static Bus createBus() {
         Price price = new Price(750000, 5);
         Bus bus = new Bus(1, "Netlab Bus", Facility.LUNCH, price, 25, BusType.REGULER, City.BANDUNG, new Station(1, "Depok Terminal", City.DEPOK, "Jl. Margonda Raya"), new Station(1, "Halte UI", City.JAKARTA, "Universitas Indonesia"));
