191e92f24e301b97804fb4b9d22378a59e91348c
Ashennwitch <119783785+Ashennwitch@users.noreply.github.com>
2023-11-03 11:34:57 +0700

    CS06 - Update main

--- src/HanifNurIlhamSanjayaJBusBR/JBus.java
+++ src/HanifNurIlhamSanjayaJBusBR/JBus.java
@@ -1,11 +1,7 @@
 package HanifNurIlhamSanjayaJBusBR;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
 import java.sql.Timestamp;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import static HanifNurIlhamSanjayaJBusBR.Algorithm.paginate;
 
@@ -19,49 +15,22 @@ import static HanifNurIlhamSanjayaJBusBR.Algorithm.paginate;
 public class JBus {
 
     public static void main(String[] args) {
-        // PT Modul 5
-        // Tes Pagination
-        Bus b = createBus();
-        List<Timestamp> listOfSchedules = new ArrayList<>();
-        listOfSchedules.add(Timestamp.valueOf("2023-7-18 15:00:00"));
-        listOfSchedules.add(Timestamp.valueOf("2023-7-20 12:00:00"));
-        listOfSchedules.add(Timestamp.valueOf("2023-7-22 10:00:00"));
-        listOfSchedules.add(Timestamp.valueOf("2023-7-26 12:00:00"));
 
-        listOfSchedules.forEach(b::addSchedule);
-        System.out.println("Page 1");
-        paginate(b.schedules, 0, 3, t -> true).forEach(System.out::println);
-        System.out.println("=====================================================");
-        System.out.println("Page 2");
-        paginate(b.schedules, 1, 3, t -> true).forEach(System.out::println);
-        System.out.println("=====================================================");
+        //Renter renter = new Renter(hanif, hanifs);
+        //System.out.println(true Regex, void validate());
 
-        // Tes Booking
-        String msgSuccess = "Booking Success!";
-        String msgFailed = "Booking Failed";
-        // valid date, invalid seat = Booking Failed
-        Timestamp t1 = Timestamp.valueOf("2023-7-19 15:00:00");
-        System.out.println("\nMake booking at July 19, 2023 15:00:00 Seats: BR17 BR18");
-        System.out.println(Payment.makeBooking(t1, List.of("BR17", "BR18"), b) ? msgSuccess : msgFailed);
-        // valid date, invalid seat = Booking Failed
-        Timestamp t2 = Timestamp.valueOf("2023-7-18 15:00:00");
-        System.out.println("Make booking at July 18, 2023 15:00:00 Seat BR26");
-        System.out.println(Payment.makeBooking(t2, "BR26", b) ? msgSuccess : msgFailed);
 
-        // valid date, valid seat = Booking Success
-        System.out.println("Make booking at July 18, 2023 15:00:00 Seats: BR7 BR8");
-        System.out.println(Payment.makeBooking(t2, List.of("BR07", "BR08"), b) ? msgSuccess : msgFailed);
-        // valid date, valid seat = Booking Success
-        Timestamp t3 = Timestamp.valueOf("2023-7-20 12:00:00");
-        System.out.println("Make booking at July 20, 2023 12:00:00 Seats: BR1 BR2");
-        System.out.println(Payment.makeBooking(t3, List.of("BR01", "BR02"), b) ? msgSuccess : msgFailed);
+        try {
+            String filepath =
+                    "C:\\Users\\Hanif\\Documents\\DEV\\Praktikum OOP\\JBus\\data\\buses_CS.json";
+            JsonTable<Bus> busList = new JsonTable<>(Bus.class, filepath);
+            List<Bus> filteredBus =
+                    filterByDeparture(busList, City.JAKARTA, 1, 10);
+            filteredBus.forEach(bus -> System.out.println(bus.toString()));
+        } catch (Throwable t) {
+            t.printStackTrace();
 
-        // valid date, book the same seat = Booking Failed
-        System.out.println("Make booking at July 20, 2023 12:00:00 Seat BR1");
-        System.out.println(Payment.makeBooking(t3, "BR1", b) ? msgSuccess : msgFailed);
-        // check if the data changed
-        System.out.println("\nUpdated Schedule");
-        paginate(b.schedules, 0, 4, t -> true).forEach(System.out::println);
+        }
     }
 
     public static Bus createBus() {
@@ -102,10 +71,11 @@ public class JBus {
 
         return filteredBuses;
     }
-//bus.getId() == id)
+
+    //bus.getId() == id)
     public static Bus filterBusId(List<Bus> buses, int id) {
         for (Bus bus : buses) {
-            if (bus.id == id){
+            if (bus.id == id) {
                 return bus;
             }
         }
@@ -117,19 +87,5 @@ public class JBus {
 
         return paginate(buses, page, pageSize, filterPredicate);
     }
-
-    Renter renter = new Renter(123, hanif, hanifs);
-
-
-    try {
-        String filepath =
-                "C:\\Users\\Hanif\\Documents\\DEV\\Praktikum OOP\\JBus\\data\\buses_CS.json";
-        JsonTable<Bus> busList = new JsonTable<>(Bus.class, filepath);
-        List<Bus> filteredBus =
-                filterByDeparture (busList, City.JAKARTA,1,10);
-        filteredBus.forEach (bus -> System.out.println(bus.toString()));
-    }
-    catch (Throwable t) {
-        t.printStackTrace();
-
 }
+
